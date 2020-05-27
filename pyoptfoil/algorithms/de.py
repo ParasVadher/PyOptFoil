c109762d9056fbbde37317125e845669354bc32a
ParasVadher <47538333+ParasVadher@users.noreply.github.com>
2020-05-27 23:52:03 +0100

    Updated Differential Evolution Class

--- pyoptfoil/algorithms/de.py
+++ pyoptfoil/algorithms/de.py
@@ -5,14 +5,17 @@ import random
 
 
 class DE:
-    def __init__(self, bounds: dict, pop_size: int, n_iterations: int, param_method: str):
+    def __init__(self, bounds: dict, pop_size: int, n_generations: int, param_method: str, f: float, cr: float):
         self.bounds = bounds
         self.pop_size = pop_size
-        self.n_iterations = n_iterations
+        self.n_generations = n_generations
         self.param_method = param_method
+        self.f = f
+        self.cr = cr
 
         self.best_individual = None
         self.population = []
+        self.best_fitness_history = []
 
         bounds_arr = np.array(self.bounds.values())
         self.lb = bounds_arr[:, 0]
@@ -42,10 +45,55 @@ class DE:
             individual.fitness = func(individual)
             if self.best_individual is None or individual.fitness > self.best_individual.fitness:
                 self.best_individual = copy.deepcopy(individual)
+                self.best_fitness_history.append(self.best_individual.fitness)
+
+    def mutate(self, idx):
+        pot_index = [i for i in range(self.pop_size) if i != idx]
+        random_index = random.sample(pot_index, 2)
+
+        x0 = self.population[idx]
+        x1 = self.population[random_index[0]]
+        x2 = self.population[random_index[1]]
+
+        v_donor = np.array(x0.position.values())
+        v_donor += self.f * (np.array(self.best_individual.position.values()) - v_donor)
+        v_donor += self.f * (np.array(x1.position.values()) - np.array(x2.position.values()))
+        return v_donor
+
+    def crossover(self, idx, v_donor):
+        v_trial = self.population[idx]
+        for k in range(len(v_donor)):
+            if np.random.random() < self.cr:
+                v_trial[k] = v_donor[k]
+
+        return v_trial
+
+    def selection(self, idx, v_trial, func):
+        target_individual = self.population[idx]
+
+        trial_params = dict(zip(self.bounds.keys(), v_trial))
+        trial_individual = Aerofoil(target_individual.name, self.param_method, trial_params)
+
+        trial_individual.fitness = func(trial_individual)
+
+        replaced = False
+        if trial_individual.fitness > target_individual.fitness:
+            self.population[idx] = trial_individual
+            replaced = True
+
+        return replaced
 
     def optimize(self, func):
         self.initialise_population()
         self.evaluate_population(func)
 
-        for iter in range(self.n_iterations):
-            raise NotImplementedError()
+        for gen in range(self.n_generations):
+            for idx in range(self.pop_size):
+                v_donor = self.mutate(idx)
+                v_trial = self.crossover(idx, v_donor)
+                replaced = self.selection(idx, v_trial, func)
+
+                if replaced and self.population[idx].fitness > self.best_individual.fitness:
+                    self.best_individual = copy.deepcopy(self.population[idx])
+
+            self.best_fitness_history.append(self.best_individual.fitness)
