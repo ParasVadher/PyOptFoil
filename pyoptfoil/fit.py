18c127b736b6074c08dcc63f4de7daceae011c44
ParasVadher <47538333+ParasVadher@users.noreply.github.com>
2020-09-08 18:54:52 +0100

    Updated fit.py

--- pyoptfoil/fit.py
+++ pyoptfoil/fit.py
@@ -8,3 +8,5 @@ def _aerofoil_similarity(x_u, y_u, x_l, y_l, aerofoil):
         y_u_trial = np.interp(x_u, aerofoil.x_u, aerofoil.y_u)
         y_l_trial = np.interp(x_l, aerofoil.x_l, aerofoil.y_l)
         return -(np.linalg.norm(y_l - y_l_trial) + np.linalg.norm(y_u - y_u_trial))
+
+
