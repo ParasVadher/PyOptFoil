30d9dcf27631c155b300c738e3003187db0e5a67
ParasVadher <47538333+ParasVadher@users.noreply.github.com>
2020-09-08 19:21:49 +0100

    Updated fit.py

--- pyoptfoil/fit.py
+++ pyoptfoil/fit.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .algorithms.de import DE
 
 
 def _aerofoil_similarity(x_u, y_u, x_l, y_l, aerofoil):
@@ -10,3 +11,6 @@ def _aerofoil_similarity(x_u, y_u, x_l, y_l, aerofoil):
         return -(np.linalg.norm(y_l - y_l_trial) + np.linalg.norm(y_u - y_u_trial))
 
 
+def fit(optimizer: DE, x_u: float, y_u: float, x_l: float, y_l: float):
+    def f(a): return _aerofoil_similarity(x_u, y_u, x_l, y_l, a)
+    optimizer.optimize(f)
