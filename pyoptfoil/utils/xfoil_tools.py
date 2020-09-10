d3401564b6d4200e713827b4bdf11df31c42759e
ParasVadher <47538333+ParasVadher@users.noreply.github.com>
2020-09-10 23:55:55 +0100

    Updated xfoil_tools.py

--- pyoptfoil/utils/xfoil_tools.py
+++ pyoptfoil/utils/xfoil_tools.py
@@ -4,7 +4,14 @@ from ..aerofoil import Aerofoil
 
 
 def write_dat(aerofoil: Aerofoil):
-    pass
+    x = np.concatenate((aerofoil.x_u, np.flip(aerofoil.x_l)))
+    y = np.concatenate((aerofoil.y_u, np.flip(aerofoil.y_l)))
+
+    lines = ['{0} {1}\n'.format(x[i], y[i]) for i in range(len(x))]
+
+    with open('xfoil.dat', 'w') as f:
+        f.write(aerofoil.name + r'\n')
+        f.writelines(lines)
 
 
 def run_xfoil(xfoil_path: str, datfile_path: str, alfas: tuple, re: float, m: float, itermax: int):
